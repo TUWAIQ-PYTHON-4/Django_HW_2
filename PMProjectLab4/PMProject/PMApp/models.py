72853c99d88a7fd4c0f9575bbaaf36eec2e75fe0
mohamed sulaiman <mohamed@outlook.sa>
2022-05-24 01:36:26 +0300

    Added a new folders

---
+++ PMProjectLab4/PMProject/PMApp/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+
+
+class Project(models.Model):
+    name = models.CharField(max_length = 110 , help_text = "The name of the Project.")
+    creation_time = models.DateTimeField(auto_now_add=True , help_text="The is creatig time")
+    completion_time = models.DateTimeField( null= False , help_text= "The is completion time")
+
+class Task(models.Model):
+    title = models.CharField(max_length= 110 , help_text= "The title of the Task.")
+    description= models.TextField( help_text= "  ")
+    time_estimate = models.IntegerField(help_text= "  ")
+    completed = models.BooleanField(default = False)
+
