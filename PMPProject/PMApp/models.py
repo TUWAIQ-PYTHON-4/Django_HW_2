c733c218d6e1eaeb9e4c424ab511fc14850481dd
ahmed <a-5amd@hotmail.com>
2022-05-24 23:05:46 +0300

    solved

--- PMPProject/PMApp/models.py
+++ PMPProject/PMApp/models.py
@@ -2,17 +2,19 @@ from django.db import models
 
 # Create your models here.
 class Project(models.Model):
-    name = models.CharField(max_length=150, help_text="project name")
-    date = models.DateField(auto_now_add=True, help_text="Date the Prject .")
-    completion_time = models.DateField(True, help_text="Completion time.")
-    def __str__(self):
-        return self.name
+   name = models.CharField(max_length=100, help_text="The name of the Project.")
+   creation_time = models.DateTimeField(auto_now_add=True, help_text = "The date and time the project was created.")
+   completion_time = models.DateTimeField(null=True, help_text="The date and time the project was completed.")
+
+   def __str__(self):
+      return self.name
 
 class Task(models.Model):
-    Project =models.ForeignKey(Project,on_delete=models.CASCADE)
-    title = models.CharField(max_length=150, help_text="title")
-    description = models.TextField(help_text="Description")
-    time_estimate = models.IntegerField(help_text="Time")
-    completed = models.BooleanField(default=False)
-    def __str__(self):
-        return self.title
+   title = models.CharField(max_length=100, help_text=" Title of the Task.")
+   description = models.TextField(help_text = "description of the Task.")
+   time_estimate = models.IntegerField(help_text=" estimate for the Task.")
+   completed = models.BooleanField(default=False)
+   project = models.ForeignKey(Project, on_delete=models.CASCADE)
+
+   def __str__(self):
+       return self.title
